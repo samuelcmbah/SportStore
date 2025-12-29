df2d45dfa62247ab72e9b540680df0e02e1a6f64
Samuel Mbah <samuelcmbah@gmail.com>
2025-12-29 13:46:56 +0100

    make some cart item model change

--- SportStore/ViewModels/CartVM/CartItem.cs
+++ SportStore/ViewModels/CartVM/CartItem.cs
@@ -3,11 +3,13 @@ namespace SportStore.ViewModels.CartVM;
 
 public class CartItem
 {
-    public int CartItemID { get; set; }
-    public Product Product { get; set; } = new();
+    public int CartItemId { get; set; }
+
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+
     public int Quantity { get; set; }
 
-    //foreign key and navigation prop
     public int CartId { get; set; }
     public Cart Cart { get; set; } = null!;
 
