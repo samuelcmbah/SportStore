33e80f2de55e9843ac6c922bc7d3e0f1cb83b853
Samuel Mbah <samuelcmbah@gmail.com>
2026-01-01 09:19:16 +0100

    separate product controller from administration controller and separate the views into respective folders

---
+++ SportStore/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+using SportStore.Services.IServices;
+using SportStore.ViewModels.ProductVM;
+
+namespace SportStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class ProductsController : Controller
+    {
+        private readonly IStoreRepository storeRepository;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ProductsController(IStoreRepository storeRepository, IWebHostEnvironment webHostEnvironment)
+        {
+            this.storeRepository = storeRepository;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult DeleteProduct(long id)
+        {
+            Product? product = storeRepository.GetProductById(id);
+            if (product == null)
+            {
+                //tell the admin that the product does not exist and return them to the avilable lisst of products
+            }
+            storeRepository.DeleteProduct(product);
+
+            ProductsListViewModel model = new ProductsListViewModel()
+            {
+                Products = storeRepository.AllProducts
+            };
+
+
+            return View("ManageProducts", model);
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string? uniqueFileName = UploadFile(model);
+                Product product = new()
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Category = model.Cartegory,
+                    Price = model.Price,
+                    PhotoPath = uniqueFileName
+                };
+
+                storeRepository.CreateProduct(product);
+                return RedirectToAction("Details", new { id = product.ProductID });
+            }
+            return View();
+        }
+
+        private string? UploadFile(ProductCreateViewModel model)
+        {
+            string? uniqueFileName = null;
+
+            if (model.Photo != null)
+            {
+                //create the upload path
+                var uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                //create unique file name
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                //create file path
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                //copy to images folder
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Photo.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
+        }
+
+        [HttpGet]
+        public IActionResult Edit(long id)
+        {
+            Product? product = storeRepository.GetProductById(id);
+            if (product == null)
+            {
+                //direct to ProductNotFound page and then to the admin llst of all products
+            }
+            ProductEditViewModel productEditViewModel = new()
+            {
+                ProductID = product.ProductID,
+                Name = product.Name,
+                Description = product.Description,
+                Cartegory = product.Category,
+                Price = product.Price,
+                ExistingPhotoPath = product.PhotoPath,
+            };
+
+            return View(productEditViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductEditViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Product? editedProduct = storeRepository.GetProductById(model.ProductID);
+                if (editedProduct != null)
+                {
+                    editedProduct.Name = model.Name;
+                    editedProduct.Description = model.Description;
+                    editedProduct.Category = model.Cartegory;
+                    editedProduct.Price = model.Price;
+                    if (model.Photo != null)
+                    {
+                        if (model.ExistingPhotoPath != null)
+                        {
+                            string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", model.ExistingPhotoPath);
+                            System.IO.File.Delete(filePath);
+                        }
+                        editedProduct.PhotoPath = UploadFile(model);
+                    }
+                }
+                storeRepository.UpdateProduct(editedProduct);
+                return RedirectToAction("Details", new { id = model.ProductID });
+            }
+            return View();
+        }
+
+        public IActionResult Details(long id)
+        {
+            Product? product = storeRepository.GetProductById(id);
+            if (product == null)
+            {
+                //direct to ProductNotFound page and then to the admin list of all products
+                Response.StatusCode = 404;
+                return View("ProductNotFound");
+            }
+
+            return View(product);
+        }
+
+        public IActionResult ManageProducts()
+        {
+            var model = new ProductsListViewModel
+            {
+                Products = storeRepository.AllProducts
+            };
+            return View(model);
+        }
+    }
+}
--- SportStore/Program.cs
+++ SportStore/Program.cs
@@ -15,133 +15,137 @@ using SportStore.Services.IServices;
 using SportStore.Utils;
 using System;
 
-namespace SportStore
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        //configuring third party logging
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration));
+        //adding email service
+        builder.Services.AddScoped<IEmailService, EmailService>();
+        builder.Services.Configure<ResendEmailSettings>(
+            builder.Configuration.GetSection("ResendEmailSettings"));
+        // This is required to use the IOptions<T> pattern
+        builder.Services.AddOptions();
+
+        builder.Services.Configure<ResendClientOptions>(o =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            o.ApiToken = builder.Configuration["ResendEmailSettings:ApiKey"]!;
+        });
 
-            // Add services to the container.
+        // 5. Add the HttpClient and the main Resend client to the services
+        builder.Services.AddHttpClient<ResendClient>();
+        builder.Services.AddTransient<IResend, ResendClient>();
+        //adding identity
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            options.SignIn.RequireConfirmedEmail = true;
+            options.User.RequireUniqueEmail = true;
+        }).AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
 
-            //configuring third party logging
-            builder.Host.UseSerilog((context, configuration) =>
-                configuration.ReadFrom.Configuration(context.Configuration));
-            //adding email service
-            builder.Services.AddScoped<IEmailService, EmailService>();
-            builder.Services.Configure<ResendEmailSettings>(
-                builder.Configuration.GetSection("ResendEmailSettings"));
-            // This is required to use the IOptions<T> pattern
-            builder.Services.AddOptions();
+        builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+            options.UseNpgsql(
+                builder.Configuration.GetConnectionString("SportsStoreConnection")));
 
-            builder.Services.Configure<ResendClientOptions>(o =>
-            {
-                o.ApiToken = builder.Configuration["ResendEmailSettings:ApiKey"]!;
-            });
+        builder.Services.AddDbContext<StoreDbContext>(options =>
+            options.UseNpgsql(
+                builder.Configuration.GetConnectionString("SportsStoreConnection")));
 
-            // 5. Add the HttpClient and the main Resend client to the services
-            builder.Services.AddHttpClient<ResendClient>();
-            builder.Services.AddTransient<IResend, ResendClient>();
-            //adding identity
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
-                options.SignIn.RequireConfirmedEmail = true;
-                options.User.RequireUniqueEmail = true;
-            }).AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+        builder.Services.AddScoped<IOrderNotificationService, OrderNotificationService>();
+        builder.Services.AddScoped<IOrderDomainService, OrderDomainService>();
+        builder.Services.AddScoped<ICartDomainService, CartDomainService>();
+        builder.Services.AddScoped<ICartService, CartService>();
+        builder.Services.AddScoped<IAccountService, AccountService>();
+        builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+        builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 
-            builder.Services.AddDbContext<AppIdentityDbContext>(options =>
-                options.UseNpgsql(
-                    builder.Configuration.GetConnectionString("SportsStoreConnection")));
-
-            builder.Services.AddDbContext<StoreDbContext>(options =>
-                options.UseNpgsql(
-                    builder.Configuration.GetConnectionString("SportsStoreConnection")));
-
-            builder.Services.AddScoped<IOrderNotificationService, OrderNotificationService>();
-            builder.Services.AddScoped<IOrderDomainService, OrderDomainService>();
-            builder.Services.AddScoped<ICartDomainService, CartDomainService>();
-            builder.Services.AddScoped<ICartService, CartService>();
-            builder.Services.AddScoped<IAccountService, AccountService>();
-            builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
-            builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
-           
-            builder.Services.AddHttpContextAccessor();
-            builder.Services.AddScoped<SessionCart>(); 
-            builder.Services.AddDistributedMemoryCache();
-            builder.Services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromMinutes(30);
-                options.Cookie.HttpOnly = true;
-                options.Cookie.IsEssential = true;
-            });
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddScoped<SessionCart>();
+        builder.Services.AddDistributedMemoryCache();
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(30);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
 
 
-            builder.Services.AddControllersWithViews();
-            //builder.Services.AddControllersWithViews(options =>
-            //{
-            //    var policy = new AuthorizationPolicyBuilder()
-            //                     .RequireAuthenticatedUser()
-            //                     .Build();
-            //    options.Filters.Add(new AuthorizeFilter(policy));
-            //});
-            builder.Services.AddAuthorization(options =>
-            {
-                options.FallbackPolicy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-            });
+        builder.Services.AddControllersWithViews();
+        //builder.Services.AddControllersWithViews(options =>
+        //{
+        //    var policy = new AuthorizationPolicyBuilder()
+        //                     .RequireAuthenticatedUser()
+        //                     .Build();
+        //    options.Filters.Add(new AuthorizeFilter(policy));
+        //});
+        builder.Services.AddAuthorization(options =>
+        {
+            options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+        });
 
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseStatusCodePagesWithReExecute("/Error/{0}");
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-            app.UseRouting();
+        app.UseRouting();
 
-            
-            app.UseSession();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+        app.UseSession();
 
-            app.MapControllerRoute(name: "catpage",
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        //AREA ROUTE (must come first)
+        app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+        );
+
+        app.MapControllerRoute(
+             name: "catpage",
              pattern: "{category}/Page{productPage:int}",
              defaults: new { Controller = "Home", action = "Index" });
 
-            app.MapControllerRoute(name: "page",
+        app.MapControllerRoute(
+            name: "page",
             pattern: "Page{productPage:int}",
             defaults: new { Controller = "Home", action = "Index", productPage = 1 });
 
-            app.MapControllerRoute(name: "category",
+        app.MapControllerRoute(
+            name: "category",
             pattern: "{category}",
             defaults: new { Controller = "Home", action = "Index", productPage = 1 });
 
-            //app.MapControllerRoute(name: "pagination",
-            //    pattern: "Products/Page{productPage:int}",
-            //     defaults: new { Controller = "Home", action = "Index", id = 1 });
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.MapDefaultControllerRoute();
+        app.MapDefaultControllerRoute();
 
-            SeedData.EnsurePopulated(app);
-            IdentitySeedData.EnsurePopulated(app);
+        SeedData.EnsurePopulated(app);
+        IdentitySeedData.EnsurePopulated(app);
 
-            app.Run();
-        }
+        app.Run();
     }
 }
