eb75f12852a5bdba7332799d9b673e8bc1e8e97b
Samuel Mbah <samuelcmbah@gmail.com>
2026-01-03 13:08:49 +0100

    refactor category controller to consume ICategoryService

--- SportStore/Areas/Admin/Controllers/CategoryController.cs
+++ SportStore/Areas/Admin/Controllers/CategoryController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SportStore.Data;
 using SportStore.Models;
 using SportStore.Services.IServices;
+using System.Threading.Tasks;
 
 namespace SportStore.Areas.Admin.Controllers
 {
@@ -10,19 +12,17 @@ namespace SportStore.Areas.Admin.Controllers
     [Authorize(Roles = "Administrator")]
     public class CategoryController : Controller
     {
-        private readonly IStoreRepository storeRepository;
-        private readonly StoreDbContext context;
+        private readonly ICategoryService categoryService;
 
-        public CategoryController(IStoreRepository storeRepository, StoreDbContext context)
+        public CategoryController(ICategoryService categoryService)
         {
-            this.storeRepository = storeRepository;
-            this.context = context;
+            this.categoryService = categoryService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var categories = storeRepository.Categories
-                .OrderBy(c => c.Name) .ToList();
+            var categories = await categoryService.GetAll()
+                .OrderBy(c => c.Name).ToListAsync();
             return View(categories);
         }
 
@@ -33,22 +33,21 @@ namespace SportStore.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category category)
+        public async Task<IActionResult> Create(Category category)
         {
             if (!ModelState.IsValid)
             {
                 return View(category);
             }
 
-            context.Categories.Add(category);
-            context.SaveChanges();
+            await categoryService.CreateAsync(category);
 
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Edit(int id)
+        public async Task<IActionResult> Edit(int id)
         {
-            var category = context.Categories.Find(id);
+            var category = await categoryService.GetByIdAsync(id);
 
             if (category == null)
             {
@@ -60,21 +59,20 @@ namespace SportStore.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category category)
+        public async Task<IActionResult> Edit(Category category)
         {
             if (!ModelState.IsValid)
             { 
                 return View(category);
             }
-            context.Categories.Update(category);
-            context.SaveChanges();
+            await categoryService.UpdateAsync(category);
 
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            var category = context.Categories.Find(id);
+            var category = await categoryService.GetByIdAsync(id);
 
             if (category == null)
             {
@@ -86,15 +84,9 @@ namespace SportStore.Areas.Admin.Controllers
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var category = context.Categories.Find(id);
-
-            if (category != null)
-            {
-                context.Categories.Remove(category);
-                context.SaveChanges();
-            }
+            await categoryService.DeleteAsync(id);
 
             return RedirectToAction(nameof(Index));
         }
