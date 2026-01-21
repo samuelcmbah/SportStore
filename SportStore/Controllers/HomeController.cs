629b6f892664aa17225fb55f7ba2c29df5f8fa83
Samuel Mbah <samuelcmbah@gmail.com>
2026-01-21 19:20:50 +0100

    display go to cart button if product is already in cart in product details page.

--- SportStore/Controllers/HomeController.cs
+++ SportStore/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SportStore.Models;
+using SportStore.Services;
 using SportStore.Services.IServices;
+using SportStore.Utils;
 using SportStore.ViewModels;
 using SportStore.ViewModels.ProductVM;
 
@@ -11,12 +13,22 @@ namespace SportStore.Controllers
     {
         private readonly ILogger<HomeController> logger;
         private readonly IProductService productService;
+        private readonly ICartService cartService;
+        private readonly SessionCart sessionCart;
+        private readonly ICurrentUserService currentUserService;
         public int ProductPerPage = 8;
 
-        public HomeController(ILogger<HomeController> logger, IProductService productService )
+        public HomeController(ILogger<HomeController> logger, 
+            IProductService productService,
+            ICartService cartService,
+            SessionCart sessionCart,
+            ICurrentUserService currentUserService)
         {
             this.logger = logger;
             this.productService = productService;
+            this.cartService = cartService;
+            this.sessionCart = sessionCart;
+            this.currentUserService = currentUserService;
         }
 
         [AllowAnonymous]
@@ -68,9 +80,29 @@ namespace SportStore.Controllers
                 return NotFound();
             }
 
-            return View(product);
+            Cart cart;
+            if (currentUserService.IsAuthenticated)
+            {
+                cart = await cartService.GetOrCreateCartByUserIdAsync(currentUserService.UserId!);
+            }
+            else
+            {
+                cart = sessionCart.GetCart();
+            }
+
+            var cartItem = cart.CartItems.FirstOrDefault(ci => ci.Product.ProductID == id);
+
+
+            var vm = new ProductDetailsViewModel
+            {
+                Product = product,
+                IsInCart = cartItem != null,
+                QuantityInCart = cartItem?.Quantity ?? 0,
+                MaxQuantity = Math.Min(product.StockQuantity, 10)
+            };
+            return View(vm);
         }
 
-
+       
     }
 }
